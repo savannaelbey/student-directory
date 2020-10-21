8c3968e1e63ec715df9a27756b0eae7424a8b3c8
savannaelbey <savanna_e@icloud.com>
2020-10-21 17:39:02 +0100

    interactive list of students

--- directory.rb
+++ directory.rb
@@ -1,33 +1,42 @@
-# create an array of hashes, each containing student data
-students= [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-# create a method to print the header
+def input_students
+  # create an empty students list
+  students = []
+  # ask user to input a name
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # get the first name
+  name = gets.chomp.capitalize
+  # while name is not empty:
+  while !name.empty?
+    # add the student hash to the empty array
+    students << {name: name, cohort: :november}
+    # print message showing how many names have been entered so far and ask for another name.
+    puts "Now we have #{students.count} students"
+    # get another name from the user and save it to the variable "name"
+    name = gets.chomp.capitalize
+  end
+  # return list of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-# create a method to print the student name and cohort
+
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
-# create a method to print the footer containing the total number of students
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-# call the methods
+
+# get list of students from user and assign the return value to the variable "students"
+students = input_students
 print_header
+# print list of student names and cohort
 print(students)
 print_footer(students)
