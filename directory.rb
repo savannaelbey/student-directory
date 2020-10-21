907e044ac1a7dfb0218616b34784aa7f9b4d3e94
savannaelbey <savanna_e@icloud.com>
2020-10-21 16:50:19 +0100

    converted students array of arrays to an array of hashes and adjusted methods

--- directory.rb
+++ directory.rb
@@ -1,24 +1,33 @@
-# create an array containing student names
-student_list = ["Dr. Hannibal Lecter", "Darth Vader",
-  "Nurse Ratched", "Michael Corleone", "Alex DeLarge",
-  "The Wicked Witch of the West", "Terminator", "Freddy Krueger", "The Joker",
-  "Joffrey Baratheon", "Norman Bates"]
+# create an array of hashes, each containing student data
+students= [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
 # create a method to print the header
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-# create a method to print each name in the student list array
-def print_names(names)
-  names.each do |name|
-    puts name
+# create a method to print the student name and cohort
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 # create a method to print the footer containing the total number of students
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 # call the methods
 print_header
-print_names(student_list)
-print_footer(student_list)
+print(students)
+print_footer(students)
